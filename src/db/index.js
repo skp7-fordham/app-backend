31ec1ff38936c4b367cd530e2d3480d62b875f4c
Shivam Patel <shivam.kpatel2604@gmail.com>
2025-03-27 19:31:25 -0400

    connection on mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();  
+
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async ()=>{
+    try {
+       const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       console.log(`/n MongoDB connected!! ${connectionInstance.connection.host}`)
+       console.log('Connection State:', connectionInstance.connection.readyState);
+    } catch (error) {
+        console.log("ERROR:",error)
+        process.exit(1);
+    }
+}
+
+export default connectDB
