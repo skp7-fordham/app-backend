e88c2a65133450e4c935d92c2e8d01d1c84a802f
Shivam Patel <shivam.kpatel2604@gmail.com>
2025-03-28 13:26:45 -0400

    utility code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@
 import dotenv from "dotenv"
 
 import mongoose from "mongoose";
+import { app } from "./app.js";
 //import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
 
 dotenv.config({path:"./env"})
+const port = process.env.PORT;
 
 connectDB()
+.then(()=>{
+   
+   app.on("error",(error)=>{
+      console.log(`error: ${error}`)
+      throw error;
+   })
+   
+   app.listen(port || 8000,()=>{
+      console.log(`Server running at ${port}`)
+   })
+})
+.catch((err)=>{
+   console.log("DB connection fail:",err)
+})
 
 /*
 import express from "express";
@@ -21,8 +37,8 @@ const app = express();
           throw error;
        })
 
-       app.listen(process.env.PORT,()=>{
-          console.log(`running on ${process.env.PORT}`);
+       app.listen(port,()=>{
+          console.log(`running on ${port}`);
        })
     } catch (error) {
         console.log("Error:"  error)
