ae14e949e3f26d9a11dccf20a77e1c0495cdae71
Shivam Patel <shivam.kpatel2604@gmail.com>
2025-04-10 11:14:24 -0400

    configuring and adding routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
-import { registerUser,loginUser,logOutUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { registerUser,loginUser,logOutUser,refreshAccessToken,
+          changePassword,currentuser,updateaccount,userAvatarUpdate,
+          userCoverimgUpdate,getUserchannelProfile,getWatchHisotry } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.js";
 import pkg from 'jsonwebtoken';  
 const { Jwt } = pkg;
@@ -23,6 +25,16 @@ router.route("/register").post(
     router.route("/login").post(loginUser)
     router.route("/logout").post(verifyJWT,logOutUser)
     router.route("/refresh-token").post(refreshAccessToken)
+    router.route("/change-password").post(verifyJWT,changePassword)
+    router.route("/current-user").get(verifyJWT,currentuser)
+    router.route("/update-account").patch(verifyJWT,updateaccount)
+    router.route("/avatar").patch(verifyJWT,upload.single("avatar"),userAvatarUpdate)
+    router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),userCoverimgUpdate)
+    router.route("/c/:username").get(verifyJWT,getUserchannelProfile)
+    router.route("/history").get(verifyJWT,getWatchHisotry)
+
+
+
 
 
 export default router
